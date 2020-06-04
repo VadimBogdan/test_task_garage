136684c197c7cbc84c66da35185efc6b01fc4208
Vadym Bogdan <vadymbog@gmail.com>
2020-06-04 10:55:18 +0300

    Initial Commit

---
+++ test_task_garage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_task_garage
+//
+//  Created by Вадим on 04.06.2020.
+//  Copyright © 2020 Vadym Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
