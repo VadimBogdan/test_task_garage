e04b102b0f745a732320ec36561a381c21112607
Vadym Bogdan <vadymbog@gmail.com>
2020-06-05 16:11:28 +0300

    edit tab bar item, delete on swipe, core data persistence.

--- test_task_garage/ContactsViewController.swift
+++ test_task_garage/ContactsViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class ContactsViewController: UIViewController, UITableViewDataSource {
 
-    let users = [
+    let defaultContacts = [
         ["Thomas", "Surname", "example@mail.example"],
         ["Lenny", "Surname", "example@example.com"],
         ["Andre", "Surname", "same@example.com"]
     ]
+    
+    var contacts = [NSManagedObject]()
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -23,15 +26,72 @@ class ContactsViewController: UIViewController, UITableViewDataSource {
         
         tableView.delegate = self
         tableView.dataSource = self
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh,
+                                                           target: self,
+                                                           action: #selector(refreshContacts))
+    }
+    
+    @objc func refreshContacts() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        for contact in contacts {
+            managedContext.delete(contact)
+        }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        contacts.removeAll()
+        fetchContacts(managedContext)
     }
     
     var lastlySelectedRowIndex: IndexPath?
     
     override func viewWillAppear(_ animated: Bool) {
-        
         if let last = lastlySelectedRowIndex {
             tableView.deselectRow(at: last, animated: true)
             lastlySelectedRowIndex = nil
+        }
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        fetchContacts(managedContext)
+    }
+    
+    fileprivate func fetchContacts(_ managedContext: NSManagedObjectContext) {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+        do {
+            contacts = try managedContext.fetch(fetchRequest)
+            if contacts.count == 0 {
+                createContacts(managedContext: managedContext)
+                try managedContext.save()
+            }
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+         tableView.reloadData()
+    }
+    
+    fileprivate func setContactProperties(_ contactData: [String], _ contact: NSManagedObject) {
+        let name = contactData[0]
+        let surname = contactData[1]
+        let email = contactData[2]
+        let image = UIImage(named: name + " " + surname)?.pngData()
+        contact.setValue(name, forKey: "name")
+        contact.setValue(surname, forKey: "surname")
+        contact.setValue(email, forKey: "email")
+        contact.setValue(image, forKey: "image")
+    }
+    
+    fileprivate func createContacts(managedContext: NSManagedObjectContext) {
+        for contactData in defaultContacts {
+            let entity = NSEntityDescription.entity(forEntityName: "Contact",
+                                                in: managedContext)!
+            let contact = NSManagedObject(entity: entity,
+                                        insertInto: managedContext)
+            setContactProperties(contactData, contact)
+            contacts.append(contact)
         }
     }
 }
@@ -39,21 +99,29 @@ class ContactsViewController: UIViewController, UITableViewDataSource {
 extension ContactsViewController: UITableViewDelegate {
     @available(iOS 2.0, *)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return contacts.count
     }
 
-       
        // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
        // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-       
-       @available(iOS 2.0, *)
+    
+    fileprivate func setContactTableCellProperties(_ cell: ContactTableViewCell, _ indexPath: IndexPath) {
+        let name = contacts[indexPath.row].value(forKeyPath: "name") as! String
+        let surname = contacts[indexPath.row].value(forKeyPath: "surname") as! String
+        let image = UIImage(data: contacts[indexPath.row].value(forKeyPath: "image") as! Data)
+        
+        cell.contactFullName.text = name + " " + surname
+        cell.contactImage.image = image
+    }
+    
+    @available(iOS 2.0, *)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell") as! ContactTableViewCell
         
         cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
         
-        cell.contactFullName.text = users[indexPath.row][0] + " " + users[indexPath.row][1]
-        cell.contactImage.image = UIImage(named: users[indexPath.row][0] + " " + users[indexPath.row][1])
+        setContactTableCellProperties(cell, indexPath)
+
         cell.contactImage.layer.cornerRadius = cell.contactImage.frame.height / 2
         return cell
     }
@@ -62,18 +130,41 @@ extension ContactsViewController: UITableViewDelegate {
         return 100
     }
     
-    
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = storyboard?.instantiateViewController(identifier: "DetailedContactViewController")
             as? DetailedContactViewController
-        
-        vc?.name = users[indexPath.row][0]
-        vc?.surname = users[indexPath.row][1]
-        vc?.email = users[indexPath.row][2]
-        vc?.image =  UIImage(named: users[indexPath.row][0] + " " + users[indexPath.row][1])
+    
+        vc?.contact = contacts[indexPath.row]
         
         lastlySelectedRowIndex = indexPath
         navigationController?.pushViewController(vc!, animated: true)
+    }
+}
+
+extension ContactsViewController {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            tableView.beginUpdates()
+            removeContact(contactIndex: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .none)
+            tableView.endUpdates()
+        }
+    }
+    
+    func removeContact(contactIndex: Int) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        managedContext.delete(contacts[contactIndex])
+        contacts.remove(at: contactIndex)
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+
     }
 }
--- test_task_garage/DetailedContactViewController.swift
+++ test_task_garage/DetailedContactViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailedContactViewController: UIViewController {
 
@@ -22,17 +23,87 @@ class DetailedContactViewController: UIViewController {
     
     var image: UIImage?
     
+    var contact: NSManagedObject?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let contact = contact {
+            name = contact.value(forKeyPath: "name") as? String
+            surname = contact.value(forKeyPath: "surname") as? String
+            email = contact.value(forKeyPath: "email") as? String
+            image = UIImage(data: contact.value(forKeyPath: "image") as! Data)
+        }
         
         imageView.image = image
         nameTextField.text = name
         surnameTextField.text = surname
         emailTextField.text = email
+
+        navigationItem.rightBarButtonItem = editButtonItem
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func setEditing(_ editing: Bool, animated: Bool) {
         
+        
+        setTextFieldsIsEnabled(editing)
+        
+        if !editing {
+            saveChanges()
+            setTextFieldsTextColor(#colorLiteral(red: 0.5568627451, green: 0.5568627451, blue: 0.5764705882, alpha: 1))
+        } else {
+            setTextFieldsTextColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        }
+        
+        super.setEditing(editing, animated: animated)
     }
-
+    
+    func saveChanges() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        if let contact = contact {
+            updateContactProperties(contact: contact)
+        } else {
+            createCustomContact(managedContext: managedContext)
+        }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func updateContactProperties(contact: NSManagedObject) {
+        contact.setValue(nameTextField.text, forKey: "name")
+        contact.setValue(surnameTextField.text, forKey: "surname")
+        contact.setValue(emailTextField.text, forKey: "email")
+        contact.setValue(imageView.image?.pngData(), forKey: "image")
+    }
+    
+    func createCustomContact(managedContext: NSManagedObjectContext) {
+        let entity =
+          NSEntityDescription.entity(forEntityName: "Contact",
+                                     in: managedContext)!
+        let contact = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        updateContactProperties(contact: contact)
+    }
+    
+    func setTextFieldsIsEnabled(_ isEnabled: Bool) {
+        nameTextField.isEnabled = isEnabled
+        surnameTextField.isEnabled = isEnabled
+        emailTextField.isEnabled = isEnabled
+    }
+    
+    func setTextFieldsTextColor(_ color: UIColor) {
+        UIView.transition(with: nameTextField, duration: 0.3, options: .transitionCrossDissolve, animations: {
+            self.nameTextField.textColor = color
+        })
+        UIView.transition(with: surnameTextField, duration: 0.3, options: .transitionCrossDissolve, animations: {
+            self.surnameTextField.textColor = color
+        })
+        UIView.transition(with: emailTextField, duration: 0.3, options: .transitionCrossDissolve, animations: {
+            self.emailTextField.textColor = color
+        })
+    }
 }
